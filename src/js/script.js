d965d6e8f327120c3402b1a786196de3f76a2bb1
Ilya <ilieturcan424@gmail.com>
2023-05-24 21:15:56 +0300

    asdfasdfdsaf

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+/* const num = 50;
+
+if (num < 49) {
+    console.log('Неправильно');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Верно');
+} */
+
+/* for(let i = 1; i < 8; i++) {
+    console.log(i);
+} */
+
+function logging(a, b) {
+    console.log(a + b);
+}
+
+logging(3, 7);
+
+logging(6, 8);
